afa29324ecd82cc4dbece8f02fca7bef08f9b6ae
Adam Selker <adamselker@gmail.com>
2019-11-14 18:14:14 -0500

    Make MidiToNum runnable, and deal with >127 velocities

--- MidiToNum.py
+++ MidiToNum.py
@@ -3,13 +3,18 @@ import sys
 import numpy as np
 import os, os.path
 
+
 def MidiToNum(midifiles):
     dict_notes = {}
     for i in midifiles:
-        midi = pretty_midi.PrettyMIDI(i)
-        piano_roll =  midi.instruments[0].get_piano_roll()
+        try:
+            midi = pretty_midi.PrettyMIDI(i)
+        except IOError:
+            continue
+        piano_roll = midi.instruments[0].get_piano_roll()
         dict_notes[i] = piano_roll
     return dict_notes
+
 
 def AllMidis(rootdir):
     midiFiles = []
@@ -18,3 +23,9 @@ def AllMidis(rootdir):
             if files.endswith(".mid"):
                 midiFiles.append(os.path.join(subdir, files))
     return midiFiles
+
+
+if __name__ == "__main__":
+    files = AllMidis(sys.argv[1])
+    notes = MidiToNum(files)
+    print(notes)
