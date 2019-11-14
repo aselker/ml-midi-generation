7cbbafca3ed8c8930291e0e71398f8e1c916353c
Adam Selker <adamselker@gmail.com>
2019-11-14 14:46:33 -0500

    Add Ode to Joy in Numpy format

---
+++ simple_songs.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import numpy as np
+
+
+def to_one_hot(xs):
+    a_s = []
+    for x in xs:
+        a = [0 for _ in range(8)]
+        if a:
+            a[x - 1] = 1
+        a_s.append(a)
+    return np.array(a_s)
+
+
+ode_to_joy = to_one_hot(
+    [
+        3,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1,
+        1,
+        2,
+        3,
+        3,
+        2,
+        2,
+        0,
+        3,
+        3,
+        4,
+        5,
+        5,
+        4,
+        3,
+        2,
+        1,
+        1,
+        2,
+        3,
+        2,
+        1,
+        1,
+    ]
+)
