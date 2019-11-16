f0667b18e0f2d6a821ca7df2238b212e5a7e5c20
Adam Selker <adamselker@gmail.com>
2019-11-16 12:33:49 -0500

    Factor out padding and input/target split

--- rnn_trainer.py
+++ rnn_trainer.py
@@ -15,11 +15,39 @@ from rnn_model import RnnModel
 assert sys.argv[1]
 assert sys.argv[2]
 
-state_size = 200
-n_layers = 4
-
 dtype = np.float32
 device = t.device("cuda") if t.cuda.is_available() else t.device("cpu")
+
+
+def prep_data(seq, data_width):
+    # Even out the lengths of input_seq and target_seq
+    # See https://towardsdatascience.com/taming-lstms-variable-sized-mini-batches-and-why-pytorch-is-good-for-your-health-61d35642972e
+    seq_lens = [len(s) for s in seq]
+    pad_token = -1
+    current_batch_size = len(seq)
+    padded = np.ones((current_batch_size, max(seq_lens), data_width)) * pad_token
+
+    seq = np.asarray(seq)
+    for i, l in enumerate(seq_lens):
+        padded[i, :l] = seq[i, :l]
+
+    # Create input and target datasets
+    input_padded = [x[:-1] for x in padded]
+    target_padded = [x[1:] for x in padded]
+
+    input_padded = t.Tensor(input_padded)
+    target_padded = t.Tensor(target_padded)
+    input_padded = input_padded.to(device)
+    target_padded = target_padded.to(device)
+
+    return input_padded, target_padded
+
+
+# Define hyperparameters
+state_size = 200
+n_layers = 4
+n_epochs = 30
+lr = 0.01
 
 all_data = pickle.load(open(sys.argv[1], "rb"))
 np.random.shuffle(all_data)
@@ -35,9 +63,6 @@ test_data = all_data[-test_data_count:]
 model = RnnModel(data_width, data_width, state_size, n_layers)
 model = model.to(device)
 
-# Define hyperparameters
-n_epochs = 30
-lr = 0.01
 
 loss_criterion = nn.SmoothL1Loss()
 optimizer = t.optim.Adam(model.parameters(), lr=lr)
@@ -53,34 +78,14 @@ for epoch in range(n_epochs):
 
     # Split training data into mini-batches
     batches = torch.utils.data.random_split(train_data, batch_sizes)
-    for seq in batches:  # TODO: How do we randomly split the data?
-        # Even out the lengths of input_seq and target_seq
-        # See https://towardsdatascience.com/taming-lstms-variable-sized-mini-batches-and-why-pytorch-is-good-for-your-health-61d35642972e
-        seq_lens = [len(s) for s in seq]
-        pad_token = -1
-        current_batch_size = len(seq)
-        padded = np.ones((current_batch_size, max(seq_lens),data_width)) * pad_token
-
-        seq = np.asarray(seq)
-        for i, l in enumerate(seq_lens):
-            padded[i, :l] = seq[i, :l]
-
-        # Create input and target datasets
-        input_padded = [x[:-1] for x in padded]
-        target_padded = [x[1:] for x in padded]
-
-        input_padded = t.Tensor(input_padded)
-        target_padded = t.Tensor(target_padded)
-        input_padded = input_padded.to(device)
-        target_padded = target_padded.to(device)
-        # TODO: We could make the model never see the pad values
-
+    for seq in batches:
+        input_padded, target_padded = prep_data(seq, data_width)
         # Run the model
         optimizer.zero_grad()
         # Initialize the state to zeros
-        state = t.zeros(n_layers, current_batch_size, state_size)
+        state = t.zeros(n_layers, len(seq), state_size)
         state = state.to(device)
-        output, _ = model(input_padded,state)
+        output, _ = model(input_padded, state)
 
         loss = loss_criterion(output.view(-1), target_padded.view(-1))
         loss.backward()
